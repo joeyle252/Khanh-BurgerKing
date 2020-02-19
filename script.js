4aab5e5ca855a64f1b3f480c95b02678458af2e2
khanh <lekimhongkhanh@gmail.com>
2020-02-19 19:56:32 +0700

    restructure git

---
+++ script.js
@@ -0,0 +1,44 @@
+
+/* const selectBurger = ["burger-button-1","burger-button-2","burger-button-3","burger-button-4","burger-button-5","burger-button-6","burger-button-7","burger-button-8"];
+selectBurger.forEach (function (item) {
+    item.classList.remove('.selected-burger')
+});
+*/
+/* 
+find all element with selector burger class 
+loop through each element and remove selector burger class from all the img
+*/
+/*
+const allSelectorBurgers = document.querySelectorAll('.selected-burger');
+allSelectorBurgers.forEach(function (item) {
+    item.classList.remove('.selected-burger')
+});
+
+const divContainer = btnElement.parentNode;
+const imageElement = divContainer.querySelector('img');
+if (imageElement.classList.contains('.selected-burger')) {
+    imageElement.classList.remove(".selected-burger")
+} else {
+    imageElement.classList.add(".selected-burger")
+}
+};
+*/
+
+//1. when a user clicks addToCart, please make the burger title red. And make ALL the other burger titles gray. 
+
+/* find all element with selected burger title class
+loop through each element and remove selected burger title class from all the title
+
+
+const allSelectorBurgerTitles = document.querySelectorAll('.selected-burger-title');
+    allSelectorBurgers.forEach(function (item) {
+        item.classList.remove('.selected-burger-title')
+    });
+
+    const divContainer = btnElement.parentNode;
+    const titleElement = divContainer.querySelector('title');
+    if (titleElement.classList.contains('.selected-burger-title')) {
+        titleElement.classList.remove(".selected-burger-title")
+    } else {
+        titleElement.classList.add(".selected-burger-title")
+*/
